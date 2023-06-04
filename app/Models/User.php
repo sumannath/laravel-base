a041b45569b134111027807c088d87fca80e32e0
Suman Nath <suman.pro@gmail.com>
2023-06-04 19:27:00 +0530

    Added migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Kra8\Snowflake\HasSnowflakePrimary;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasSnowflakePrimary, HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
